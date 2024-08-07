bfafafa79a9b3c89eaee20da8cfa9b27cb4b65ac
Thiago Berrutti <thiagoberrutti@gmail.com>
2024-08-07 13:20:41 -0300

    warnings fix

--- app/src/main/java/com/example/algorithmvisualizer/presentation/sort/SortScreen.kt
+++ app/src/main/java/com/example/algorithmvisualizer/presentation/sort/SortScreen.kt
@@ -1,10 +1,7 @@
 package com.example.algorithmvisualizer.presentation.sort
 
 import android.content.res.Configuration
-import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,58 +12,33 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconButtonColors
-import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.NonRestartableComposable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.window.Dialog
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.rememberNavController
-import com.example.algorithmvisualizer.R
-import com.example.algorithmvisualizer.domain.util.SortOperationInfoGenerator
-import com.example.algorithmvisualizer.domain.model.ISortOperation
+import com.example.algorithmvisualizer.domain.model.SortAlgorithmName
 import com.example.algorithmvisualizer.domain.model.quicksort.QuickSortAction
 import com.example.algorithmvisualizer.domain.model.quicksort.QuickSortOperation
-import com.example.algorithmvisualizer.domain.model.SortAlgorithmName
 import com.example.algorithmvisualizer.presentation.components.BarChart
 import com.example.algorithmvisualizer.presentation.sort.components.AlgorithmNameDialog
 import com.example.algorithmvisualizer.presentation.sort.components.SortControls
@@ -235,8 +207,6 @@ fun SortScreen(
         )
     }
 }
-
-
 
 
 @Preview(
